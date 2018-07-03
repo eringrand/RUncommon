392cf75b4d21e92441ea366467b9b5e88ec8ae0e
Erin Grand <eringrand@gmail.com>
2018-07-03 10:13:29 -0400

    - Edits to documentation

--- R/common_functions.R
+++ R/common_functions.R
@@ -1,5 +1,8 @@
 #' @title read_clean_data
 #' @description Reads in clean data from excel using janitor package and readxl
+#' - Read in the data, assumes na = "",
+#' - Removes empty columns and rows
+#' - Cleans column names
 #'
 #' @param file The location of the file you are using.
 #' @param sheename The name of number of the sheet (as in readxl)
@@ -43,6 +46,8 @@ tochar <- function(x) {
 #' @title colorgorical
 #' @description uses Colorgoical to create color paletes that work well.
 #' Requires httr and jsonlite packages.
+#'
+#' Credit to \href{https://gist.github.com/slowkow/22daea426607416bfcd573ce9cbd89ab}{slowkrow}
 #'  @export
 colorgorical <- function(n = 10) {
   post_body <- jsonlite::toJSON(
@@ -72,22 +77,38 @@ colorgorical <- function(n = 10) {
 
 
 #' Cohort Tag
+#' @description Defines a student's cohort given the student's grade and current school year.
+#'
+#' If you do not provide a school year the default is the current (spring) year.
+#' e.g If today's date is Jan 10, 2017, the default year will be 2017,
+#' however if the current date is Sep 10, 2017, the year will default to 2018.
+#'
 #' @param grade Enrolled grade of the student
-#' @param school_year School year either in 20XX-YY or 20XX format.
+#' @param school_year School year either in 20XX-YY or 20XX (spring) format.
 #' @export
 
 cohort <- function(grade, school_year="") {
   years_to_grade <- 12 - as.numeric(grade)
+
   if (all(school_year == "")) {
+    month_of_year <- format(Sys.Date(), "%m")
+    if(month_of_year <= 8) {
     current_year <- as.numeric(format(Sys.Date(), "%Y"))
+    } else{
+    current_year <- as.numeric(format(Sys.Date(), "%Y")) + 1
+    }
   }
+
   else {
     current_year <- as.numeric(paste0("20", stringr::str_sub(school_year, -2, -1)))
   }
+
   current_year + years_to_grade
 }
 
 #' yes_no
+#' @description Changes all 1s to YES and everything else to NO. Useful for when you have 1 or 0s flags.
+#' Beware of NAs!
 #' @export
 yes_no <- function(x) {
   dplyr::if_else(x %in% 1, "Yes", "No")
@@ -98,7 +119,7 @@ yes_no <- function(x) {
 #' @description change first_name last_name, to last_name, first_name
 #' Does not work with two part last names as it assumes only one space
 #' between the first and last name.
-#' @param name The full name of someone in the form of First Name, Last Name. A character.
+#' @param name The full name of someone in the form of First Name, Last Name.
 #' @export
 
 change_firstlast_to_lastfirst <- function(name) {
@@ -111,4 +132,16 @@ change_firstlast_to_lastfirst <- function(name) {
     x <- paste(name_list[3], name_list[1], sep = ", ")
   }
   return(x)
+}
+
+
+#' @title round_percent
+#' @description  A version of scales::percent() that allows for rounded digit
+#' so that you can have 20% or 20.5% as needed.
+#' @param x a numeric vector to format
+#' @param dig the number of digits after the percent to round. Default to 1.
+#' @export
+
+round_percent <- function(x, dig = 1) {
+  paste0(round(x * 100, digits = dig), "%")
 }
